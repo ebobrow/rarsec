5fb0964b12d824e0d8a2a1427183847b90600219
Elliot Bobrow <elliotgreybobrow@gmail.com>
2023-08-15 16:30:13 -0700

    proof of concept

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub type Parser<T> = Box<dyn Fn(&str) -> Vec<(T, &str)>>;
+
+pub fn empty() -> Parser<()> {
+    Box::new(move |input| vec![((), input)])
+}
+
+pub fn character(x: char) -> Parser<char> {
+    Box::new(move |input| {
+        if input.starts_with(x) {
+            vec![(x, &input[1..])]
+        } else {
+            Vec::new()
+        }
+    })
+}
+
+pub fn alternative<T: 'static>(f: Parser<T>, g: Parser<T>) -> Parser<T> {
+    Box::new(move |input| match f(input) {
+        xs if xs.is_empty() => g(input),
+        xs => xs,
+    })
+}
+
+pub fn sequence<T: Clone + 'static, U: 'static>(f: Parser<T>, g: Parser<U>) -> Parser<(T, U)> {
+    Box::new(move |input| {
+        let mut out = Vec::new();
+        for (ftree, rest) in f(input) {
+            for (gtree, rest) in g(rest) {
+                out.push(((ftree.clone(), gtree), rest));
+            }
+        }
+        out
+    })
 }
 
 #[cfg(test)]
@@ -7,8 +38,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn recognizers() {
+        let input = "hello";
+        assert_eq!(empty()(input), vec![((), "hello")]);
+        assert_eq!(character('h')(input), vec![('h', "ello")]);
+    }
+
+    #[test]
+    fn combinators() {
+        let input = "hello";
+        assert_eq!(
+            alternative(character('x'), character('h'))(input),
+            vec![('h', "ello")]
+        );
+        assert_eq!(
+            sequence(character('h'), character('e'))(input),
+            vec![(('h', 'e'), "llo")]
+        );
     }
 }
