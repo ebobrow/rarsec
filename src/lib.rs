f83db6db2dbc2dd37c12ad4f48e5b69c3881140a
Elliot Bobrow <elliotgreybobrow@gmail.com>
2023-08-15 19:39:06 -0700

    du macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,45 @@
-pub type Parser<T> = Box<dyn Fn(&str) -> Vec<(T, &str)>>;
+// TODO: make `x <-` optional (may require proc_macro)
+#[macro_export]
+macro_rules! du {
+    ( $x:ident <- $f:expr; $( $y:ident <- $g:expr; )* %return $ret:expr; ) => {
+        Box::new(move |input| {
+            let ($x, rest) = $f(input)?;
+            $(
+                let ($y, rest) = $g(rest)?;
+            )*
+            Some(($ret, rest))
+        })
+    };
+}
+
+pub type Parser<T> = Box<dyn Fn(&str) -> Option<(T, &str)>>;
 
 pub fn empty() -> Parser<()> {
-    Box::new(move |input| vec![((), input)])
+    Box::new(move |input| Some(((), input)))
 }
 
 pub fn character(x: char) -> Parser<char> {
     Box::new(move |input| {
         if input.starts_with(x) {
-            vec![(x, &input[1..])]
+            Some((x, &input[1..]))
         } else {
-            Vec::new()
+            None
         }
     })
 }
 
 pub fn alternative<T: 'static>(f: Parser<T>, g: Parser<T>) -> Parser<T> {
     Box::new(move |input| match f(input) {
-        xs if xs.is_empty() => g(input),
-        xs => xs,
+        None => g(input),
+        Some(xs) => Some(xs),
     })
 }
 
 pub fn sequence<T: Clone + 'static, U: 'static>(f: Parser<T>, g: Parser<U>) -> Parser<(T, U)> {
     Box::new(move |input| {
-        let mut out = Vec::new();
-        for (ftree, rest) in f(input) {
-            for (gtree, rest) in g(rest) {
-                out.push(((ftree.clone(), gtree), rest));
-            }
-        }
-        out
+        let (ftree, rest) = f(input)?;
+        let (gtree, rest) = g(rest)?;
+        Some(((ftree, gtree), rest))
     })
 }
 
@@ -40,8 +50,8 @@ mod tests {
     #[test]
     fn recognizers() {
         let input = "hello";
-        assert_eq!(empty()(input), vec![((), "hello")]);
-        assert_eq!(character('h')(input), vec![('h', "ello")]);
+        assert_eq!(empty()(input), Some(((), "hello")));
+        assert_eq!(character('h')(input), Some(('h', "ello")));
     }
 
     #[test]
@@ -49,11 +59,27 @@ mod tests {
         let input = "hello";
         assert_eq!(
             alternative(character('x'), character('h'))(input),
-            vec![('h', "ello")]
+            Some(('h', "ello"))
         );
         assert_eq!(
             sequence(character('h'), character('e'))(input),
-            vec![(('h', 'e'), "llo")]
+            Some((('h', 'e'), "llo"))
         );
+    }
+
+    #[test]
+    fn do_notation() {
+        fn hello() -> Parser<Vec<char>> {
+            du! {
+                h <- character('h');
+                e <- character('e');
+                l <- character('l');
+                l2 <- character('l');
+                o <- character('o');
+                %return vec![h, e, l, l2, o];
+            }
+        }
+
+        assert_eq!(hello()("hello"), Some((vec!['h', 'e', 'l', 'l', 'o'], "")));
     }
 }
